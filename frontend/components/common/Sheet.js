30190eca89378fed4733ccb31f47f678d6227dc8
김채윤 <only363me@gmail.com>
2022-07-25 11:12:30 +0900

    no message

---
+++ frontend/components/common/Sheet.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function BoxSx() {
+  return (
+    <Box 
+        sx={{
+        width: 300,
+        height: 300,
+        backgroundColor: 'white',
+        box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px,
+        }}
+    )
+}
+
+    
